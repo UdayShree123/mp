b6637a7eeb71010d627cda48211b9a621160d410
udayshree123 <udayshree123@gmail.com>
2020-10-01 18:55:01 +0530

    1

---
+++ src/main/java/simple1/App.java
@@ -0,0 +1,11 @@
+package simple1;
+public class App {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
